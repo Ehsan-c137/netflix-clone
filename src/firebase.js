f897b773d8c2444e0d9aa92cf3086e836ea2b742
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-02-28 13:01:31 +0330

    change import path to VITE convention

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-   apiKey: import.meta.env.REACT_APP_FIREBASE_API_KEY,
-   authDomain: import.meta.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-   projectId: import.meta.env.REACT_APP_FIREBASE_PROJECT_ID,
-   storageBucket: import.meta.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-   messagingSenderId: import.meta.env.REACT_APP_MESSAGING_SENDER,
-   appId: import.meta.env.REACT_APP_APP_ID,
+   apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+   authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+   projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
+   storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+   messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER,
+   appId: import.meta.env.VITE_APP_APP_ID,
 };
 
 // Initialize Firebase
