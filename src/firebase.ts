a7f5a3d0d5e0d2a37faf56007edd10776bf59a8d
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-03-05 22:18:48 +0330

    change import path based on vite

---
+++ src/firebase.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { Firestore, getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+   apiKey: import.meta.env.VITE_APP_FIREBASE_API_KEY,
+   authDomain: import.meta.env.VITE_APP_FIREBASE_AUTH_DOMAIN,
+   projectId: import.meta.env.VITE_APP_FIREBASE_PROJECT_ID,
+   storageBucket: import.meta.env.VITE_APP_FIREBASE_STORAGE_BUCKET,
+   messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER,
+   appId: import.meta.env.VITE_APP_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export default auth;
+export const db = getFirestore(app);
