eb4f78575b046b3722d7fab2d0e278b4ab199d52
Ehsan <74349698+Ehsan-c137@users.noreply.github.com>
2024-03-31 20:20:21 +0330

    add Carousel component

--- src/componets/Row.tsx
+++ src/componets/Row.tsx
@@ -1,14 +1,35 @@
 import axios from "axios";
 import Movie from "./Movie";
 import React, { useEffect, useState } from "react";
-import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 import PicureSkeleton from "./Skeleton";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 interface IRowProps {
    title: string;
    fetchURL: string;
    rowID: number;
 }
+
+const carouselConfig = {
+   superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 7,
+   },
+   desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 6,
+   },
+   tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 3,
+   },
+   mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 2,
+   },
+};
 
 const Row = ({ title, fetchURL, rowID }: IRowProps) => {
    const [movies, setMovies] = useState([]);
@@ -19,32 +40,31 @@ const Row = ({ title, fetchURL, rowID }: IRowProps) => {
       });
    }, [fetchURL]);
 
-   const slideLeft = () => {
-      const slider = document.getElementById("slider" + rowID) as HTMLElement;
-      slider.scrollLeft = slider.scrollLeft - 500;
-   };
-   const slideRight = () => {
-      const slider = document.getElementById("slider" + rowID) as HTMLElement;
-      slider.scrollLeft = slider.scrollLeft + 500;
-   };
+   // const slideLeft = () => {
+   //    const slider = document.getElementById("slider" + rowID) as HTMLElement;
+   //    slider.scrollLeft = slider.scrollLeft - 500;
+   // };
+   // const slideRight = () => {
+   //    const slider = document.getElementById("slider" + rowID) as HTMLElement;
+   //    slider.scrollLeft = slider.scrollLeft + 500;
+   // };
 
    return (
       <>
          <h2 className="text-white font-bol md:text-xl p-4">{title}</h2>
          <div className="relative flex items-center group">
-            <MdChevronLeft
-               onClick={slideLeft}
-               size={40}
-               className="bg-white rounded-full left-0 absolute opacity-50 hover:opacity-100 cursor-pointer right-8 z-[100] hidden group-hover:block"
-            />
-            <div
-               id={"slider" + rowID}
+            <Carousel
+               responsive={carouselConfig}
                className="w-full h-full overflow-x-scroll overflow-y-hidden whitespace-nowrap scroll-smooth scrollbar-hide relative"
+               // autoPlay={this.props.deviceType !== "mobile" ? true : false}
+               keyBoardControl={true}
+               containerClass="carousel-container"
+               removeArrowOnDeviceType={["tablet", "mobile"]}
+               // deviceType={this.props.deviceType}
+               dotListClass="custom-dot-list-style"
             >
                {movies.length > 0 ? (
-                  movies.map((item, id) => {
-                     return <Movie item={item} key={id} />;
-                  })
+                  movies.map((item, id) => <Movie item={item} key={id} />)
                ) : (
                   <div className="flex gap-4">
                      {Array.from({ length: 7 })
@@ -54,12 +74,7 @@ const Row = ({ title, fetchURL, rowID }: IRowProps) => {
                         ))}
                   </div>
                )}
-            </div>
-            <MdChevronRight
-               onClick={slideRight}
-               size={40}
-               className="bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-[100] hidden group-hover:block"
-            />
+            </Carousel>
          </div>
       </>
    );
